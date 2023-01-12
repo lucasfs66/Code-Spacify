8cd587e62f6a07517e9e0bd352a83bd3c7937da3
Lucas <lucasfreiresabino@gmail.com>
2023-01-12 10:56:45 -0700

    complete js code

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const spacify = (string) => {
     let lettersArray = string.split("")
     let stringResult = ""
-    for(let i = 0; i < lettersArray; i++){
+    for(let i = 0; i < lettersArray.length; i++){
         if(i < 1){
-            stringResult = stringResult + lettersArray[i]
+            stringResult = `${lettersArray[i]}`
         }else if(lettersArray[i] !== " "){
-            stringResult = stringResult + " " + lettersArray[i]
+            stringResult = `${stringResult} ${lettersArray[i]}`
         }
+        
     }
 
     console.log(stringResult)
